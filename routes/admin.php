1045d098ff7b5e7aab9de64f1d2a5ee7cc7f46ab
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-21 22:01:58 +0600

    Admin Route Added

---
+++ routes/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register admin routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "admin" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    dd('Welcome to admin user routes.');
+});
