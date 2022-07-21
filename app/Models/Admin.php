4eb8a829a35d7aeb3628e58910b64f2d7e051402
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-21 22:17:06 +0600

    Permission Added

---
+++ app/Models/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Model
+{
+    use HasFactory, HasRoles;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'mobile',
+        'password',
+    ];
+}
