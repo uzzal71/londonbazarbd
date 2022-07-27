46adb2fb6532db23f94970d53797dd4a11f657a8
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-27 23:05:21 +0600

    Footer Page Update

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-<footer class="text-center text-lg-start bg-black text-muted">
+<footer class="text-center text-lg-start bg-black text-muted" style="background-color: #333e4f">
   <!-- Section: Social media -->
   <section
     class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
@@ -36,15 +36,14 @@
       <!-- Grid row -->
       <div class="row mt-3">
         <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            Need Help?
           </h6>
-          <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
+          <p>Call us 01727-111 000</p>
+          <p>Email us info@londonbazarbd.com</p>
+          <p>Office Address 165, Suuvra Sagar Building, Ground Floor, New Elephant Road, Hatirpool, Dhaka-1205</p>
         </div>
         <!-- Grid column -->
 
@@ -52,19 +51,16 @@
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
-            Products
+            Related Page
           </h6>
           <p>
-            <a href="#!" class="text-reset">Angular</a>
+            <a href="#!" class="text-reset">Returns & Refunds Policy</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">React</a>
+            <a href="#!" class="text-reset">Terms & Condition</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Laravel</a>
+            <a href="#!" class="text-reset">Privacy Policy</a>
           </p>
         </div>
         <!-- Grid column -->
@@ -73,36 +69,17 @@
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
+            Get in Touch
           </h6>
           <p>
-            <a href="#!" class="text-reset">Pricing</a>
+            <a href="#!" class="text-reset">Facebook</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Settings</a>
+            <a href="#!" class="text-reset">Twitter</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Orders</a>
+            <a href="#!" class="text-reset">Linked</a>
           </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Contact
-          </h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            info@example.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
         </div>
         <!-- Grid column -->
       </div>
@@ -111,10 +88,18 @@
   </section>
   <!-- Section: Links  -->
 
+  <!-- Section -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <img src="{{ asset('uploads/payment-gateway-webhub.png') }}" alt="Payment Method" width="100%">
+    </div>
+  </section>
+  <!-- Section -->
+
   <!-- Copyright -->
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    <a class="text-reset fw-bold" href="#">Copyright © 2014 London Bazar BD</a>
+    <a class="text-reset fw-bold" href="#">Copyright © 2022 Stitch Shop</a>
   </div>
   <!-- Copyright -->
 </footer>
-<!-- Footer -->
+<!-- Footer
