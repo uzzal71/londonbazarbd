594caaa517c564fc63e914c7c3c7e1221648e67b
uzzal71 <uzzalroy.acm@gmail.com>
2022-07-30 00:25:47 +0600

    App.blade.php

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,147 @@
+<section>
+    <div class="overlay"></div>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light main-menu" style="box-shadow:none; background-color: #0083c4 !important; border-bottom: 1px solid #fff">
+  <div class="container-fluid">
+
+    <button type="button" id="sidebarCollapse" class="btn btn-link d-block d-md-none">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </button>
+
+    <a class="navbar-brand" href="#">
+      <img src="{{ asset('uploads/logo/logo.png') }}" alt="Logo" style="height: 55px;">
+    </a>
+
+    <ul class="navbar-nav ml-auto d-block d-md-none">
+      <li class="nav-item">
+        <a class="btn btn-link" href="#">
+            <i class="bx bxs-cart icon-single"></i> <span class="badge badge-danger">3</span></a>
+      </li>
+    </ul>
+
+    <div class="collapse navbar-collapse">
+      <form class="form-inline my-2 my-lg-0 mx-auto">
+        
+      </form>
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="btn btn-link" href="#">
+            <i class="fa fa-cart-arrow-down" aria-hidden="true"></i> <span class="badge badge-danger">3</span></a>
+        </li>
+        <li class="nav-item ml-md-3">
+          <a class="btn btn-primary" href="#"><i class="fa fa-user mr-1"></i> Log In / Register</a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</nav>
+
+<nav class="navbar navbar-expand-md" style="background-color: #006fa6; border-bottom: 1px solid #fff">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">ছেলেদের শপিং</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ইলেকট্রনিক্স আইটেম</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">স্বাস্থ্য সুরক্ষা পণ্য</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">গৃহস্থালি প্রোডাক্ট</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">মেয়েদের শপিং</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">বেবী অ্যান্ড কিডস</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">কসমেটিক</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">মোবাইল ও গ্যাজেট</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ক্যামেরা</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ঘড়ি</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link btn btn-warning" href="#">All Offers</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="search-bar d-block d-md-none">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <form class="form-inline mb-3 mx-auto">
+          <input class="form-control" type="search" placeholder="Search for products..." aria-label="Search">
+          <button class="btn btn-success" type="submit"><i class="bx bx-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Sidebar -->
+<nav id="sidebar">
+  <div class="sidebar-header">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-10 pl-0">
+          <a class="btn btn-primary" href="#"><i class="bx bxs-user-circle mr-1"></i> Log In</a>
+        </div>
+
+        <div class="col-2 text-left">
+          <button type="button" id="sidebarCollapseX" class="btn btn-link">
+                <i class="fa fa-times" aria-hidden="true"></i>
+            </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <ul class="list-unstyled components links">
+    <li class="active">
+      <a href="#">ছেলেদের শপিং</a>
+    </li>
+    <li>
+      <a href="#">ইলেকট্রনিক্স আইটেম</a>
+    </li>
+    <li>
+      <a href="#">স্বাস্থ্য সুরক্ষা পণ্য</a>
+    </li>
+    <li>
+      <a href="#">গৃহস্থালি প্রোডাক্ট</a>
+    </li>
+    <li>
+      <a href="#">মেয়েদের শপিং</a>
+    </li>
+    <li>
+      <a href="#">বেবী অ্যান্ড কিডস</a>
+    </li>
+    <li>
+      <a href="#">মোবাইল ও গ্যাজেট</a>
+    </li>
+    <li>
+      <a href="#">ক্যামেরা</a>
+    </li>
+    <li>
+      <a href="#">ঘড়ি</a>
+    </li>
+  </ul>
+
+</nav>
+</section>
--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -2,13 +2,13 @@
   <div class="splide__track">
         <div class="splide__list">
             <div class="splide__slide" id="splide04-slide01" tabindex="-1">
-                <img src="{{ asset('uploads/slider/Eid-banner-londonbazar.webp') }}" alt="">
+                <img src="{{ asset('uploads/slider/Eid-banner-londonbazar.webp') }}" alt="Slider 1" height="300px">
             </div>
             <div class="splide__slide" id="splide04-slide02" tabindex="-1">
-                <img src="{{ asset('uploads/slider/banner1.webp') }}" alt="">
+                <img src="{{ asset('uploads/slider/banner1.webp') }}" alt="Slider 2" height="300px">
             </div>
             <div class="splide__slide" id="splide04-slide03" tabindex="-1">
-                <img src="{{ asset('uploads/slider/banner22.webp') }}" alt="">
+                <img src="{{ asset('uploads/slider/banner22.webp') }}" alt="Slider 3" height="300px">
             </div>
         </div>
   </div>
